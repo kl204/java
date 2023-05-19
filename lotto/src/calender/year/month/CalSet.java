65ae006cbb36c9b1a187304a87727f2b2b7db94e
금정산2_PC18 <2_PC18@DESKTOP-P81F6K5>
2023-05-19 18:51:54 +0900

    1일차 로또볼

---
+++ lotto/src/calender/year/month/CalSet.java
@@ -0,0 +1,24 @@
+package calender.year.month;
+
+import java.util.ArrayList;
+
+public class CalSet {
+
+
+}
+
+/*
+ 	ArrayList<CalData> Cal = new ArrayList<>();
+	private String[] Day = {"일","월","화","수","목","금","토"};
+	
+	public CalData[] setCal() {
+		CalData[] set = null;
+		
+		return set;
+	}
+	
+	public void getCal() {
+		
+	} 
+
+ */
---
+++ lotto/src/calender/year/month/Calender.java
@@ -0,0 +1,26 @@
+package calender.year.month;
+
+public class Calender {
+	
+	
+
+
+}
+
+/*
+ 	public static void main(String[] args) {
+		Calender cal = new Calender();
+		cal.calView();
+		
+	}
+	
+	public void ready() {
+		
+		
+		
+	}
+	
+	public void calView() {
+		
+	}
+*/
