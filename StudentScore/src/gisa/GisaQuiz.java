a0eccb8ce69b6410d2bb232bcbef7f1cbb612c99
금정산2_PC18 <2_PC18@DESKTOP-P81F6K5>
2023-05-24 20:13:06 +0900

    5일차 기사퀴즈 강사님 버전

--- StudentScore/src/gisa/GisaQuiz.java
+++ StudentScore/src/gisa/GisaQuiz.java
@@ -6,7 +6,9 @@ public class GisaQuiz {
 	private ArrayList<StudentDTO> dataList;
 	
 	public GisaQuiz(ArrayList<StudentDTO> dataList) {
-		this.dataList = dataList;
+		this.dataList = dataList; //인자가 있는 생성자가 있을 때에는 인자가 없는 디폴트 생성자를 만들수가 없기 때문에 생성자에 매개변수를 주고
+									// 생성자를 만들어 버리면 데이터를 써야만 하는 클래스에 대해서 데이터가 없는 상태에서 클래스를 생성하는
+									// 상황을 방지해줄 수 있다.
 	}
 	
 	public String solveQuiz1() {
