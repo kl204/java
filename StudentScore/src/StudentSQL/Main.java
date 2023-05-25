47064ab71ca173eb7853c6d34a2a7cec20ea1a9b
금정산2_PC18 <2_PC18@DESKTOP-P81F6K5>
2023-05-25 17:49:12 +0900

    6일차 StringBuffer 수정 및 주석작업

--- StudentScore/src/StudentSQL/Main.java
+++ StudentScore/src/StudentSQL/Main.java
@@ -45,25 +45,31 @@ public class Main {
 	private ArrayList<Student> readyData() throws Exception {
 		BufferedReader br = new BufferedReader(new FileReader("resource/Abc1115.txt"));
 		ArrayList<Student> dataList = new ArrayList<Student>(); // 파싱해서 보내져아할 리스트
-		int rows;
+		
 		
 		Connection conn  = null; 
 	
 		try {
-			//jdbc Driver 등록
+																//jdbc Driver 등록
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
-			//연결하기
+																//연결하기
 			conn = DriverManager.getConnection(
 					"jdbc:mysql://localhost:3306/students",
 					"root",
 					"0000");
+																// 전송할 쿼리 문자열 준비
 			
-		String sql = "INSERT INTO student(stdNo, email, kor, eng, math, sci, hist, totalScore, mngCode, accCode, locCode)" + "VALUES(?,?,?,?,?,?,?,?,?,?,?)";
-		
+		StringBuffer bf = new StringBuffer("INSERT INTO student(stdNo, email, kor, eng, math, sci, hist, totalScore, mngCode, accCode, locCode)");		//StringBuffer 사용하는 것으로 수정
+		bf.append("VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+			
+		String sql = bf.toString();
+			
+		//String sql = "INSERT INTO student(stdNo, email, kor, eng, math, sci, hist, totalScore, mngCode, accCode, locCode)" + "VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+																// 쿼리 보낼 PreparedStatement 준비
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 
-		ResultSet resultSet = pstmt.executeQuery("select * from student where stdNo=990001");				
+		ResultSet resultSet = pstmt.executeQuery("select * from student where stdNo=990001");		// ResultSet(java.sql.ResultSet)은 executeQuery(String sql)을 통해 쿼리 실행, 결과값 저장 클래스, getInt(index) 와 같이 불러올수 있다		
 
 		resultSet.next(); // 질의문 전송 하고 result 커서를 다음으로 넘겨주어야 한다.
 		
@@ -72,81 +78,52 @@ public class Main {
 			
 		while(true) {
 			Student setStudent = new Student();
-			String str = br.readLine();
-			int changeNum;
-			String course;
-			int[] courseArr = new int[6];
-			String getChar;
-			int j = 0; // 과목 배열 수
-//			int cnt = 3;
+			String str = br.readLine();							// resource/Abc1115.txt 의 텍스트를 한줄씩 불러옴
+			String course;										// 과목 점수 문자열
+			int[] courseArr = new int[6];						// 과목들의 점수를 저장하는 int형 배열
+			String getChar;										// A, B, C 와 같이 단일 문자 temp 변수
+			int j = 0; 											// 과목 배열 수
+
 			
 			if(str==null) break;			
-		
-			changeNum = Integer.parseInt(str.substring(0, 6));
-			setStudent.setStdNum(changeNum); 					//stdNo
-			
-			pstmt.setString(1, str.substring(0, 6));
-			
-			setStudent.setEmail(str.substring(6, 10));			//email
-			pstmt.setString(2, str.substring(6, 10));
-
-			
-			for(int i = 10 ; i < 26; i+=3) {
-				
-				course = str.substring(i,i+3).trim();
-				
-				courseArr[j] = Integer.parseInt(course);
-			
-				j++;
-//				cnt++;
-			}
-			setStudent.setCourseScore(courseArr);				// kor, eng, math, sci, hist
-			
-			for(int i = 0; i < 5; i++) {
-				pstmt.setString(i+3, Integer.toString(setStudent.getCourseScore(i)));
-
-			}
-			
-
-
-			
-			changeNum = Integer.parseInt(str.substring(25, 28).replaceAll("\\s", "0"));
-			setStudent.setTotalScore(changeNum);				// totalScore
-			pstmt.setString(8, str.substring(25, 28).replaceAll("\\s", "0"));
-
-		
-			getChar = str.substring(28, 29);
-			setStudent.setProfCode(getChar);					//mngCode
-			pstmt.setString(9, getChar);
-			
-			getChar = str.substring(29, 30);
-			setStudent.setAchv(getChar);						//accCode
-			pstmt.setString(10, getChar);
-			
-			
-			getChar = str.substring(30, 31);
-			setStudent.setLocalCode(getChar);					//locCode
-			pstmt.setString(11, getChar);
-			
-			
-			dataList.add(setStudent); // 한명의 학생 행 추가
-			
-			
-			 pstmt.executeUpdate();
+					
+			pstmt.setString(1, str.substring(0, 6));			//stdNo
 						
-		}
+			pstmt.setString(2, str.substring(6, 10));			//email
 		
-		}
-		
-		
-		
-//		rows = pstmt.executeUpdate();
-//		System.out.println("저장된 행 수 : "+ rows);
-		// sql 쿼리 조회에 대한 결과 값 받는 클래스 : Resultset resultSet = pstmt.executeQuery("sql 질의 쿼리문");
-		
+			for(int i = 10 ; i < 26; i+=3) {								
+				course = str.substring(i,i+3).trim();							
+				courseArr[j] = Integer.parseInt(course);			
+				j++;
+			}
+						
+			for(int i = 0; i < 5; i++) {
+				pstmt.setString(i+3, Integer.toString(setStudent.getCourseScore(i))); // kor, eng, math, sci, hist	
+
+				}
+					
+				pstmt.setString(8, str.substring(25, 28).replaceAll("\\s", "0"));		// totalScore
 
 		
-		pstmt.close();
+				getChar = str.substring(28, 29);					
+				pstmt.setString(9, getChar);											//mngCode
+			
+				getChar = str.substring(29, 30);						
+				pstmt.setString(10, getChar);											//accCode
+			
+			
+				getChar = str.substring(30, 31);				
+				pstmt.setString(11, getChar);											//locCode
+						
+				dataList.add(setStudent); // 한명의 학생 행 추가
+						
+				pstmt.executeUpdate();
+						
+			}
+		
+		}
+		
+		pstmt.close();	//PreparedStatement 닫기
 		br.close(); // BufferedReader와 연결된 FileReader도 닫음
 		
 		}catch(ClassNotFoundException e){
